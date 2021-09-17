1fde49fc4243b4c2fe54abdfa54d287a2a54e122
m12watanabe1a <m12watanabe1a@gmail.com>
2021-09-17 11:25:13 +0900

    Implement pseudo inverse

--- src/pseudo_inverse.cpp
+++ src/pseudo_inverse.cpp
@@ -1,7 +1,29 @@
 #include <Eigen/QR>
 #include <iostream>
 
-int main(void) {
-  std::cout << "Hello World" << std::endl;
+void printArray(const Eigen::MatrixXd &m)
+{
+  for (int i = 0; i < m.cols(); i++)
+  {
+    for (int j = 0; j < m.rows(); j++)
+    {
+      std::cout << m(i, j) << " ";
+    }
+    std::cout << std::endl;
+  }
+}
+
+int main(void)
+{
+  Eigen::MatrixXd m(2, 2);
+  m(0, 0) = 3.0;
+  m(1, 0) = 2.5;
+  m(0, 1) = -1;
+  m(1, 1) = 4;
+
+  auto pinv = m.completeOrthogonalDecomposition().pseudoInverse();
+
+  printArray(m);
+  printArray(pinv);
   return 0;
 }
